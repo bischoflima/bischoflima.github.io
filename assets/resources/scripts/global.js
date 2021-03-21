c6c7a782979bcb12a16cf7611a32e4992bff9935
jose everaldo bischof de lima junior <bischoflima@gmail.com>
2021-03-21 13:27:02 -0300

    criada a pagina de login e iniciada a pagina de cadastro com materialize

---
+++ assets/resources/scripts/global.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.tooltipped').tooltip();
+});
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+function modificaCor(cor) {
+    $('.modal').modal('close');
+}
